953d0c664ceca166cb3d6205ba965b5f01865d44
MarcoBeraldo <beraldomar@gmail.com>
2022-07-22 16:32:10 +0200

    dynamic header try

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <div id="app">
 
+  
+  <div id="app">
+    
+    <HeaderSection />
   </div>
+    
 </template>
 
 <script>
+  import HeaderSection from "./components/HeaderSection.vue";
 export default {
-
-}
+  name:"App",
+  components: {
+    HeaderSection,
+  
+}}
 </script>
 
 <style>
-
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", "Open Sans", Helvetica, sans-serif;
+}
 </style>
--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,16 +1,56 @@
 <template>
   <section id="header">
-    
+    <div class="container">
+        <div>
+
+            <figure>
+                <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <nav>
+            <ul>
+                <li v-for="link in data" 
+                v-bind:key="link.text">{{ link.text }}</li>
+                
+            </ul>
+        </nav>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import '../assets/data/data';
+import data from "../assets/data/data";
 export default {
+    name: "HeaderSection",
+    data() {
+   return {
+    data,
+   }}}
 
-}
 </script>
 
-<style>
+<style scoped>
+#header{
+    padding:10px;
+}
 
+.container{
+    max-width: 1600px;
+    margin: 0 auto;
+}
+.container>div{
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
+ul{
+    list-style-type: none;
+    display: flex;
+}
+
+ul>li{
+    margin: 0 20px;
+    font-size: 0.9rem;
+    font-weight: bold;
+}
 </style>
