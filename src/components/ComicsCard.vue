132ce5c4442a77ac673684272bf99afee65aa82d
MarcoBeraldo <beraldomar@gmail.com>
2022-07-25 17:20:05 +0200

    completed exercise

---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,38 @@
+<template>
+    
+       
+
+        <figure >
+            <img :src="comic.thumb" alt="">
+            <figcaption>{{comic.series}}</figcaption>
+
+        </figure>
+        
+
+</template>
+
+<script>
+
+export default {
+    name: "ComicsCard",
+    props:{
+        comic: Object
+    }
+    }
+</script>
+
+<style scoped>
+
+figure{
+    width: calc(100% / 6);
+    margin-top:30px;
+}
+img{
+    height: 200px;
+}
+figcaption{
+    display:block;
+    font-size: 0.8rem;
+    width: 50%;
+}
+</style>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="main">
+    <div class="jumbotron"></div>
     
+        <h3 class="label">CURRENT SERIES</h3>
+        <div class="content">
+          <div class="container">
+            <div class="gallery">
+              <ComicsCard v-for="(comic,index) in comics" :key="index" :comic="comic" />
+            </div>
 
-        <div class="content"><div class="container">-->ContentGoes Here </div>
+          </div>
+        </div>
 
-      
 
-    </div>
   </div>
 </template>
 
 <script>
+import comics from "../assets/data/dc-comics.json";
+import ComicsCard from "../components/ComicsCard.vue";
 export default {
     name: "MainSection",
-    
+    data(){
+    return{
+        comics,
+    }
+ },
+    components: {
+      ComicsCard,
+    }    
 }
 </script>
 
 <style scoped>
 #main .content{
-    height: 150px;
     background-color: black;
     display: flex;
     justify-content: center;
@@ -27,7 +41,35 @@ export default {
     color:white;
     font-size: 30px;
     font-weight: bold;
+    padding: 30px;
 }
+ .jumbotron{
+  height: 600px;
+  background-image: url("../assets/img/jumbotron.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+ }
 
+ .container{
+  max-width: 1200px;
+  margin: 0 auto;
+ }
+
+ h3.label{
+  display:inline-block;
+  position: absolute;
+  bottom:550px;
+  left: 250px;
+  background-color: #0282F9;
+  color: white;
+  padding: 5px;
+  font-size: 1.5rem;
+ }
+
+ .gallery{
+    display: flex;
+    flex-wrap: wrap;
+}
 
 </style>
